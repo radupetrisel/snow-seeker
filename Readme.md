6e964a3df76bc7819151e288b663de1b12e90acc
Radu Petrisel <radupetrisel@gmail.com>
2023-08-03 17:41:24 +0300

    Implement challenge #1

--- Readme.md
+++ Readme.md
@@ -3,3 +3,7 @@
 This is project 19 of 100 Days of SwiftUI by Paul Hudson (link [here](https://www.hackingwithswift.com/books/ios-swiftui/snowseeker-introduction)). This is an app to let users browse ski resorts around the world, to help them find one suitable for their next holiday.
 
 As usual, at the end of the guided tutorial there will be some challenges for me to implement by myself.
+
+## Challenges
+
+**Challenge #1**: Add a photo credit over the ResortView image. The data is already loaded from the JSON for this purpose, so you just need to make it look good in the UI.
--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -21,9 +21,21 @@ struct ResortView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
-                Image(decorative: resort.id)
-                    .resizable()
-                    .scaledToFit()
+                ZStack {
+                    Image(decorative: resort.id)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            
+                            Text(resort.imageCredit)
+                                .font(.caption)
+                        }
+                    }
+                }
                 
                 HStack {
                     if horizontalSizeClass == .compact && dynamicTypeSize > .large {
