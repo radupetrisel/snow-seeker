8480c0219ab748ebfc42c6145e0dc6d77213633b
Radu Petrisel <radupetrisel@gmail.com>
2023-08-03 10:59:51 +0300

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Radu Petrisel on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
